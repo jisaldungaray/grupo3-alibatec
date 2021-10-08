2ba5041ef0c124c290b016176893d60e2cf6b10f
JoaquinBar1 <joa.barrionuevo@gmail.com>
2021-10-08 20:47:55 -0300

    HTML ProductDetail + fotos generales

--- proyectoIntegrador- Alibatec/app.js
+++ proyectoIntegrador- Alibatec/app.js
@@ -7,6 +7,11 @@ app.use(express.static('public'));
  
 // Rutas aca abajo
 
+app.get('/producto',(req, res ) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+});
+
 
 // Hasta aca
+
 app.listen(3000, (res, req) => console.log('Servidor corriendo, host 3000'));

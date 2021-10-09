f683554c57cfc5c6cd85b5b631f50cbb9e52749a
rosscalpe <ross.gusteban@gmail.com>
2021-10-09 17:02:25 -0300

    agregado el header con su estilo, modifique una ruta y unas carpetas, listo para seguir trabajando

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.use(express.static((path.resolve(__dirname,'public'))));
+ 
+// Rutas aca abajo
+
+app.get ('/', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'./views/index.html'))
+   });
+
+app.get('/producto',(req, res ) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+});
+
+// Hasta aca
+
+app.listen (4000, () => {
+    console.log ('servidor corriendo, Host 4000')
+});

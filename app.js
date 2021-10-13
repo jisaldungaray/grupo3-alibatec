59d98b4afda4f49f3996758906e1063f3c2650ce
jisaldungaray <jisaldungaray@gmail.com>
2021-10-13 00:29:25 -0300

    Creacion de Codigo en Registro.html

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get('/login',(req, res ) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
 
+app.get('/register',(req, res ) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
 // Hasta aca
 
 app.listen (4000, () => {

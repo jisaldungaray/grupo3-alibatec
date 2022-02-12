0e418d15c841ae6da58635d2010f917abb7affa4
Joaquin Barrionuevo <joaquinbarrionuevo@MacBook-Air-de-Joaquin.local>
2022-02-12 17:46:33 -0300

    correccion require apiscontroller

--- src/routes/APIS/productsApiRoutes.js
+++ src/routes/APIS/productsApiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productApiController = require('../../src/controllers/API/productApiController');
+const productApiController = require('../../controllers/APIS/productsApiController');
 
 router.get('/', productApiController.list);
 router.get('/:id', productApiController.detail);
